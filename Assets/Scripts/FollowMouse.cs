c94c41ffe496fd4ad51318b6b76d937f5f709d5a
Swetlana <shaban.swetlana@gmail.com>
2022-12-08 13:11:16 +0100

    first commit

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 pos = Input.mousePosition;
+        pos.z = 1;
+
+        transform.position = Camera.main.ScreenToWorldPoint(pos);
+
+    }
+}
